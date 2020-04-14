7cc8e3be47a1dc48f29c4ea70c296c3766746ba0
Thomas Dewitte <28838900+dewittethomas@users.noreply.github.com>
2020-04-15 00:47:46 +0200

    Cleaned up my code

--- text_to_speech/text_to_speech.py
+++ text_to_speech/text_to_speech.py
@@ -1,15 +1,26 @@
 from gtts import gTTS
 from playsound import playsound
+from mutagen.mp3 import MP3
 import os
+import time
 
-filename = 'temp.mp3'
+file = "speech.mp3"
 
-def speak(text, language):
-    tts = gTTS(text, lang=language)
-    tts.save(filename)
-    playsound(filename)
-    os.remove(filename)
-        
+def play(file):
+    playsound(file, True)
 
+def get_length(file):
+    audio = MP3(file)
+    return audio.info.length
 
-   
+def speak(text, lang):
+    tts = gTTS(text=text, lang=lang)
+    tts.save(file)
+
+    play(file)
+    
+    length = get_length(file)
+    time.sleep(length)
+
+    os.remove(file)
+
