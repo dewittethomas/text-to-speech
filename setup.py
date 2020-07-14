4952d74140d650ecab8f25dc892e6ed8b4ee6644
Thomas Dewitte <28838900+dewittethomas@users.noreply.github.com>
2020-07-14 23:58:33 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     author_email="thomasdewittecontact@gmail.com",
 
     name='text_to_speech',
-    version='1.2.2',
+    version='1.2.3',
     license="MIT",
     url='https://github.com/dewittethomas/text-to-speech',
     python_requires='>= 3.5',
